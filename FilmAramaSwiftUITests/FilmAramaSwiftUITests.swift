9b2f8b33b53d1076f8669402333b05250d828305
ebrarmangan <ebrar.mangan03@gmail.com>
2024-11-05 13:05:47 +0300

    Initial Commit

---
+++ FilmAramaSwiftUITests/FilmAramaSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  FilmAramaSwiftUITests.swift
+//  FilmAramaSwiftUITests
+//
+//  Created by EbrarMN on 5.11.2024.
+//
+
+import Testing
+@testable import FilmAramaSwiftUI
+
+struct FilmAramaSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
